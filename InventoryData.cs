205c1629dbab71232d8dad2fec0bd6e25d6f89e6
Cezary <cez94k@gmail.com>
2020-05-13 19:46:49 +0200

    Delete lines starting with hash method

---
+++ InventoryData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Inventory_Sorter
+{
+    public class InventoryData
+    {
+        public InventoryData(string warehouse, int totalMaterialAmount)
+        {
+            Warehouse = warehouse;
+            TotalMaterialAmount = totalMaterialAmount;
+        }
+        public string Warehouse { get; set; }
+        public int TotalMaterialAmount { get; set; }
+        public Material Material { get; set; }
+    }
+
+    public class Material
+    {
+        public Material(string materialId, int materialAmount)
+        {
+            MaterialId = materialId;
+            MaterialAmount = materialAmount;
+        }
+        public string MaterialId { get; set; }
+        public int MaterialAmount { get; set; }
+    }
+}
