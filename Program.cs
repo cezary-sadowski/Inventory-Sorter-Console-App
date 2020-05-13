17e286af927f30871661fdefa5a749d2d8ab2b12
Cezary <cez94k@gmail.com>
2020-05-13 17:06:32 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inventory_Sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
